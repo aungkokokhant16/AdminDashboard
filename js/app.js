978b4f1ef91a9f0bb696b7fbb167ac9199a71a4d
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-02-12 18:53:54 +0630

    sidebar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(".show-sidebar-btn").click(function() {
+    $(".sidebar").animate({ marginLeft: 0 });
+});
+
+$(".hide-sidebar-btn").click(function() {
+    $(".sidebar").animate({ marginLeft: "-100%" });
+});
