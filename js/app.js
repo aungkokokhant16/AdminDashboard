30c71ecb88f7df30b9129ad5d59d4decd70515c3
Aung Ko Ko Khant <aungkokokhant16@gmail.com>
2022-02-25 00:11:51 +0630

    customize div box to other size with javascript

--- js/app.js
+++ js/app.js
@@ -5,3 +5,9 @@ $(".show-sidebar-btn").click(function() {
 $(".hide-sidebar-btn").click(function() {
     $(".sidebar").animate({ marginLeft: "-100%" });
 });
+
+function go(url) {
+    setTimeout(function() {
+        location.href = `${url}`;
+    }, 500);
+}
